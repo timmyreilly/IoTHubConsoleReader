d2f5de693edb880731800fe03054c27620a80b8f
TimmyReilly <timreilly@live.com>
2016-05-20 10:04:24 -0700

    initial commit

---
+++ ReadDeviceToCloudMessages/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging; 
+
+namespace ReadDeviceToCloudMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "HostName=YOURHUBNAME.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=YOURSECRETOKENqffPEGwfxnDgWNRFZ400=";
+            string iotHubD2cEndpoint = "messages/events";
+
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            foreach(string partition in d2cPartitions)
+            {
+                var receiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.Now);
+                ReceiveMessagesFromDeviceAsync(receiver);
+            }
+            Console.ReadLine(); 
+        }
+
+        async static Task ReceiveMessagesFromDeviceAsync(EventHubReceiver receiver)
+        {
+            while (true)
+            {
+                EventData eventData = await receiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Messages received: '{0}'", data); 
+            }
+        }
+
+    }
+}
